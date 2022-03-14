00c64318c52c046088fa2ca4fff328e8ff3c17a1
Gahyun <applemeansyou@gmail.com>
2022-03-14 22:55:31 +0900

    계산기 앱 설명 추가(2022.03.14)

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 
+// 열거형(enum)으로 연산 묶기
 enum Operation {
     case Add
     case Subtract
@@ -34,7 +35,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    // 숫자 클릭
     @IBAction func tapNumberButton(_ sender: UIButton) {
         guard let numberValue = sender.titleLabel?.text else { return }
         if self.displayNumber.count < 9 {
@@ -43,6 +44,7 @@ class ViewController: UIViewController {
         }
     }
     
+    // AC 클릭
     @IBAction func tapClearButton(_ sender: UIButton) {
         self.displayNumber = ""
         self.firstOperand = ""
@@ -51,33 +53,43 @@ class ViewController: UIViewController {
         self.currentOperation = .unknown
         self.numberOutputLabel.text = "0"
     }
+    // . 클릭
     @IBAction func tapDotButton(_ sender: UIButton) {
         if self.displayNumber.count < 8, !self.displayNumber.contains(".") {
             self.displayNumber += self.displayNumber.isEmpty ? "0." : "."
             self.numberOutputLabel.text = self.displayNumber
         }
     }
+    // ÷ 클릭
     @IBAction func tapDivideButton(_ sender: UIButton) {
         self.operation(.Divide)
     }
+    // * 클릭
     @IBAction func tapMultiplyButton(_ sender: UIButton) {
         self.operation(.Multiply)
     }
+    // - 클릭
     @IBAction func tapSubtractButton(_ sender: UIButton) {
         self.operation(.Subtract)
     }
+    // + 클릭
     @IBAction func tapAddButton(_ sender: UIButton) {
         self.operation(.Add)
     }
+    // = 클릭
     @IBAction func tapEqualButton(_ sender: UIButton) {
         self.operation(self.currentOperation)
     }
+    
+    // 연산 함수
     func operation(_ operation : Operation){
+        // 처음 입력하지 않았을 경우 ( = 두번째 연산자 입력 시)
         if self.currentOperation != .unknown{
             if !self.displayNumber.isEmpty{
                 self.secondOperand = self.displayNumber
                 self.displayNumber = ""
                 
+                // 문자인 입력 값을 숫자로 만들어주기
                 guard let firstOperand = Double(self.firstOperand) else { return }
                 guard let secondOperand = Double(self.secondOperand) else { return }
                 
@@ -98,7 +110,7 @@ class ViewController: UIViewController {
                     break
                     
                 }
-                
+                // 소수점 자리가 없을 때는 1의 자리 숫자까지만 보여주기
                 if let result = Double(self.result), result.truncatingRemainder(dividingBy: 1) == 0 {
                     self.result = "\(Int(result))"
                 }
@@ -109,7 +121,8 @@ class ViewController: UIViewController {
             }
             self.currentOperation = operation
             
-        } else {
+        } else  {
+            // 처음 입력한 값 저장하기
             self.firstOperand = self.displayNumber
             self.currentOperation = operation
             self.displayNumber = ""
